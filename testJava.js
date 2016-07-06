495d2f933a63139acb6831ccb76f987601cfb31e
Hyein <hyeinu65@gmail.com>
2016-07-06 00:59:32 -0400

    Update testJava.js

--- testJava.js
+++ testJava.js
@@ -1,24 +1,10 @@
-$.ajax({
-  url: "http://httpbin.org/post",
-  success: createDiv,
+$("profileBtn").click(function{
+	$.getJSON ("https://api.github.com/users/hyeinu", function(key, value){
+		var avatar = $('<img />', {
+			id: 'myAvatar',
+			src: value.avatar_url,
+			width: 100,
+		});
+	avatar.appendTo(divProfile);
+	});
 });
-
-function createDiv() {
-  var divContainer = document.createElement("div");
-  divContainer.className = "container";
-  var divAvatar = document.createElement("div");
-  $.get('http://api.github.com/users/hyeinu', function(value) {
-    var avatar = $('<img />', {
-      id: 'myAvatar',
-      src: value.avatar_url,
-      width: 150,
-    });
-    avatar.appendTo(divAvatar);
-    var username = $('<h1>' + value.login + '</h1>')
-    var name  = $('<h2>' + value.name + '</h2>')
-	username.appendTo(divAvatar);
-    name.appendTo(divAvatar);
-  })
-  divContainer.appendChild(divAvatar)
-  document.body.appendChild(divContainer)
-}
